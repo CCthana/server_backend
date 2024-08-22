fbcfc457aaf37d0a920ab6e2a0e9bfb8bf8841e7
Chatchai Thanatammatat <163982803+CCthana@users.noreply.github.com>
2024-08-22 15:49:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-
+const PORT = 8005
 
 
 const app = express();
@@ -11,6 +11,6 @@ app.get('/', (req, res, next) => {
    res.send('Hello fromm express');
 });
 
-app.listen(process.env.PORT, () => {
-   console.log('Running at port:', process.env.port)
+app.listen(PORT, () => {
+   console.log('Running at port:', PORT)
 })
